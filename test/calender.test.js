07d0787dc9a6fa88de490bb6d3bf658faa30ff83
Emil Larsen <71249469+0xcyber-sketch@users.noreply.github.com>
2022-01-29 14:04:58 +0100

    Added a test framework
    Added a test framework. Doesn't work yet

---
+++ test/calender.test.js
@@ -0,0 +1,19 @@
+import calender from '../models/calender.js'
+
+describe("My callender", () => {
+
+    beforeEach(() => {
+        const c = calender()
+    }) 
+
+    it('Calender is created empty', () => {
+        console.log(c);
+
+
+
+    } )
+
+    it.todo('calender add tasks')
+
+    it.todo('Calender tasks')
+})
