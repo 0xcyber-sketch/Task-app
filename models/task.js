d33a90bb1ffe5d3f303a9ef4da1cf51874d5cff9
Emil Larsen <71249469+0xcyber-sketch@users.noreply.github.com>
2022-02-07 17:29:54 +0100

    Update task.js

--- models/task.js
+++ models/task.js
@@ -42,7 +42,26 @@ const  task = (() => {
         }
 
         addDay(d) {
-            this.#days.push(d)
+            if(typeof(d) !== 'number') throw new Error("Input day is not a number")
+            if (d < 0) throw new Error("Day has to be greater than 0")
+            if (this.#findDaySearch(d) === -1 ) {
+                this.#days.push(Math.floor(d))
+            }
+            
+        }
+
+        #findDaySearch(x) {
+            let index = -1
+            let i = 0
+            while (index === -1 && i < this.#days.length) {
+                if (this.#days[i] === x) {
+                    index = i
+                }
+                else {
+                    i++
+                }
+            }
+            return index
         }
 
         // Delete task
