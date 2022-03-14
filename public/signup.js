9836e29b116fe6e2d8f9145698982f078d0d36d0
Emil Larsen <71249469+0xcyber-sketch@users.noreply.github.com>
2022-03-14 23:03:12 +0100

    Update signup.js
    Added redirct on succesfylly created a user

--- public/signup.js
+++ public/signup.js
@@ -3,4 +3,7 @@ async function signup() {
     
     let data = {username : document.querySelectorAll(".Signup-container input")[0].value, mail : document.querySelectorAll(".Signup-container input")[1].value, confirmMail : document.querySelectorAll(".Signup-container input")[2].value, password : document.querySelectorAll(".Signup-container input")[3].value }
     let res = await post(data, "/signup")
+    if (res.status === 201) {
+        window.location.href = "http://localhost:8080/login"
+    }
 }
